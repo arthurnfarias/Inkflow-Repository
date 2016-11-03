f49fd353d697d45b260091bb48c3f1c4b8de2373
arthurnfarias <anf2@cin.ufpe.br>
2016-11-03 14:44:20 -0300

    Migration deletada

--- db/migrate/20161103170448_remove_details_from_user.rb
+++
@@ -1,7 +0,0 @@
-class RemoveDetailsFromUser < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :first_name, :string
-    remove_column :users, :last_name, :string
-    remove_column :users, :username, :string
-  end
-end
