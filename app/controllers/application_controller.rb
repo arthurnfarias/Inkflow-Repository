0c8b41c46cb1523b60209cb03fc4c0b20b00d1b2
arthurnfarias <anf2@cin.ufpe.br>
2016-11-03 14:37:28 -0300

    Correção dos controllers do devise e application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
-  protected
   
-  def configure_permitted_parameters
-  	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :email, :password, :remember_me)} 
-	devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:email, :password, :remember_me)} 
-	devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :username, :email, :password, :current_password, :remember_me)} 
-  	
-  end
 
 end
